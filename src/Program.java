33f0842e76ebcfd3d213b6f5d6f7d76a7c5a63a3
tal azouri <tal.az.gm@mail.com>
2017-05-31 10:39:53 +0300

    test commit

---
+++ src/Program.java
@@ -0,0 +1,12 @@
+/**
+ * Created by tal azouri on 27/05/2017.
+ */
+public class Program {
+
+
+    public static void main(String[] args){
+
+        System.out.println("hello");
+    }
+
+}
---
+++ src/test.java
@@ -0,0 +1,6 @@
+/**
+ * Created by tal azouri on 31/05/2017.
+ */
+public class test
+{
+}
